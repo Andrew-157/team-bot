69388fbc19d8a365a5ec89be44e887c2b3331ddf
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-22 17:35:34 +0200

    класи для записів
    Name, Phone, Email, Address, Note, Birthday,  Record, поки без AddressBook та методів

--- bot_assistant/classes.py
+++ bot_assistant/classes.py
@@ -1,5 +1,5 @@
 from collections import UserDict
-
+import re
 
 """
 Класи бота помічника.
@@ -23,7 +23,14 @@ class Record:
     телефонів класу Phone. Поле Birthday створюється пустим, поле Email створюється порожній список в якому будуть
     екземпляри класу EmailContact, поле
     """
-    pass
+
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phone = []
+        self.notes = []
+        self.birthday = None
+        self.address = None
+        self.email = None
 
 
 class AddressBook(UserDict):
@@ -38,14 +45,18 @@ class Field:
     """
     Батьківський клас для Name, Phone, Birthday, AddressContact, EmailContact.
     """
-    pass
+
+    def __init__(self, value):
+        self.value = value
 
 
 class Name(Field):
     """
     Ім'я контакта.
     """
-    pass
+
+    def __init__(self, name):
+        super().__init__(name)
 
 
 class Phone(Field):
@@ -53,7 +64,23 @@ class Phone(Field):
     Номер телефону контакта.
     Додається до списку phones, який створюється при ініціалізації класу Record.
     """
-    pass
+
+    def __init__(self, phone):
+        self.__value = None
+        super().__init__(phone)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, phone):
+        check_match = re.search(
+            r"\([0-9]{2}\)\-[0-9]{3}\-[0-9]{1}\-[0-9]{3}|\([0-9]{2}\)\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}", phone)
+        if check_match:
+            self.__value = phone
+        else:
+            self.__value = None
 
 
 class Birthday(Field):
@@ -61,7 +88,22 @@ class Birthday(Field):
     День народження контакта.
     Додається до списку birthday, який створюється при ініціалізації класу Record.
     """
-    pass
+
+    def __init__(self, birthday):
+        self.__value = None
+        super().__init__(birthday)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, birth_day):
+        check_match = re.search(r"[0-9]{4}\-[0-9]{2}\-[0-9]{2}", birth_day)
+        if check_match:
+            self.__value = birth_day
+        else:
+            self.__value = None
 
 
 class AddressContact(Field):
@@ -69,7 +111,9 @@ class AddressContact(Field):
     Адрес контакта.
     Додається до списку address, який створюється при ініціалізації класу Record.
     """
-    pass
+
+    def __init__(self, address):
+        super().__init__(address)
 
 
 class EmailContact(Field):
@@ -77,7 +121,23 @@ class EmailContact(Field):
     Email контакта.
     Додається до списку email_contact, який створюється при ініціалізації класу Record.
     """
-    pass
+
+    def __init__(self, email):
+        self.__value = None
+        super().__init__(email)
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, email):
+        check_match = re.search(
+            r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+", email)
+        if check_match:
+            self.__value = email
+        else:
+            self.__value = None
 
 
 class Notes(Field):
@@ -85,4 +145,7 @@ class Notes(Field):
     Нотатки до контакта.
     Додаються до списку notes_contact, який створюється при ініціалізації класу Record.
     """
-    pass
+
+    def __init__(self, note, tag):
+        super().__init__(note)
+        self.tag = tag
